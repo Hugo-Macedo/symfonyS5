68f4c4a221b0e5b69abb9c5c4e5c410beea796f2
Hugo <macedohugo77@gmail.com>
2024-03-02 17:48:24 +0100

    feat: adding of userId in jwt payload

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+
+class JWTCreatedListener 
+{
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    /**
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @param JWTCreatedEvent $event
+     *
+     * @return void
+     */
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $request = $this->requestStack->getCurrentRequest();
+
+        $user = $event->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+
+        $payload       = $event->getData();
+        $payload['id'] = $user->getId();
+
+        $event->setData($payload);
+
+        $header        = $event->getHeader();
+        $header['cty'] = 'JWT';
+
+        $event->setHeader($header);
+    }
+}
